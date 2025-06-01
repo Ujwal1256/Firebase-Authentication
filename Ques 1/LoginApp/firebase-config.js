8b6a85541e81c9945a48bb31154e9a2fd73a825c
Ujwal1256 <jakhamateujwal1256@gmail.com>
2025-06-01 22:34:57 +0530

    authenticatioon

---
+++ Ques 1/LoginApp/firebase-config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD1MoTp6KCr_0ol-93IHtrTa-x57ku52NY",
+  authDomain: "registerapp-4800e.firebaseapp.com",
+  databaseURL: "https://registerapp-4800e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "registerapp-4800e",
+  storageBucket: "registerapp-4800e.firebasestorage.app",
+  messagingSenderId: "801722975045",
+  appId: "1:801722975045:web:522af5df371d5c16f8f42c",
+  measurementId: "G-7QW70QNP7Y"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
